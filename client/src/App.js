610ff5c7e9d6d6f78758ba92167fe46a7238890b
christiechindy <88538204+christiechindy@users.noreply.github.com>
2022-12-12 13:44:19 +0800

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Movie from "./components/Movie";
+import Home from "./pages/Home";
+
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
+        // <div className="App">
+        //     <Header/>
+        //     <div className="jarak" style={{height: "100px"}}></div>
+        //     <Movie/>
+
+        //     <Footer/>
+        // </div>
         <div className="App">
             <Header/>
-            <div className="jarak" style={{height: "100px"}}></div>
-            <Movie/>
-
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
             <Footer/>
         </div>
     );
